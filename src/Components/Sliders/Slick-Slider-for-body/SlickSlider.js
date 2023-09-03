8a84f5732e1d6ae764be46f2674e67482fde3d5d
tikesh027 <117527667+tikesh027@users.noreply.github.com>
2023-09-04 00:25:19 +0530

    Update SlickSlider.js

--- src/Components/Sliders/Slick-Slider-for-body/SlickSlider.js
+++ src/Components/Sliders/Slick-Slider-for-body/SlickSlider.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import "./SlickSlider.css";
